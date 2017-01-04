7f8a715a3cedb0a2e09fabd215e723a0d6d88efa
AJ Funk <aj.w.funk.9@gmail.com>
2017-01-03 18:11:30 -0800

    fetch a single organization

--- src/organization.js
+++ src/organization.js
@@ -3,18 +3,31 @@ import Q from 'q';
 
 export default function organization(): object {
 
-  let url = `organizations`;
-
-  function sendRequest(endpoint: string, cb: object): undefined {
+  function sendRequest(endpoint: string, options: object = {}, cb: object): undefined {
+    let url = `organizations${(endpoint ? `/${endpoint}?` : '?')}`;
+    if (options) {
+      if (options.query) url = `${url}query=${options.sort}&`;
+      if (options.limit) url = `${url}limit=${options.sort}&`;
+      if (options.offset) url = `${url}offset=${options.sort}`;
+    }
     axios.get(url)
-      .then((response: object): object => cb(null, response.data.RECDATA))
+      .then((response: object): object => cb(null, response.data))
       .catch((err: object): object => cb(err));
   }
 
   return {
-    all(options: object): undefined {
+    all(options: object = {}): undefined {
       const deferred = Q.defer();
-      sendRequest(null, (err: string, data: object): undefined => {
+      sendRequest(null, options, (err: string, data: object): undefined => {
+        if (err) return deferred.reject(err);
+        return deferred.resolve(data);
+      });
+      return deferred.promise;
+    },
+
+    organization(organizationID: string): undefined {
+      const deferred = Q.defer();
+      sendRequest(organizationID, null, (err: string, data: object): undefined => {
         if (err) return deferred.reject(err);
         return deferred.resolve(data);
       });
--- tests/organization.spec.js
+++ tests/organization.spec.js
@@ -2,6 +2,12 @@ import RIDB from 'index';
 
 describe('RIDB.organization.all()', () => {
   it('returns array', () =>
-    RIDB.organization.all().then((data) => expect(data).to.be.instanceof(Array))
+    RIDB.organization.all().then((data) => expect(data.RECDATA).to.be.instanceof(Array))
+  );
+});
+
+describe('RIDB.organization.organization()', () => {
+  it('returns array', () =>
+    RIDB.organization.organization(128).then((data) => expect(data).to.be.instanceof(Array))
   );
 });
